3df6a58a1419ef8ee9c2666d9e84c339be6f4011
Nolway <faizeau.alexis@gmail.com>
2022-03-18 19:23:25 +0100

    Add Moralis IPFS Uploader

--- src/uploaders/MoralisUploader.ts
+++ src/uploaders/MoralisUploader.ts
@@ -1,12 +1,34 @@
+import axios from "axios";
 import { ConfigIpfsMoralis } from "../guards/ConfigGuards";
-import { FileBuffer, UploadResult } from "../guards/UploaderGuard";
+import { FileBuffer, isMoralisUploaderResult, UploadResult } from "../guards/UploaderGuard";
 import { GenericUploader } from "./GenericUploader";
 
 export class MoralisUploader implements GenericUploader {
+	private readonly apiUrl = "https://deep-index.moralis.io/api/v2";
 
-    public async upload(config: ConfigIpfsMoralis, files: FileBuffer[], folder: string): Promise<UploadResult> {
-        return {
-            hash: "",
-        };
-    }
+	public async upload(config: ConfigIpfsMoralis, files: FileBuffer[], folder: string): Promise<UploadResult> {
+		const data = [];
+
+		for (const file of files) {
+			data.push({
+				path: `${folder}/${file.name}`,
+				content: file.buffer.toString("base64"),
+			});
+		}
+
+		const result = await axios.post(`${this.apiUrl}/ipfs/uploadFolder`, data, {
+			maxBodyLength: Infinity,
+			headers: {
+				"X-API-KEY": config.auth.key,
+				"Content-Type": "application/json",
+				accept: "application/json",
+			},
+		});
+
+		const moralisResult = isMoralisUploaderResult.parse(result.data);
+
+		return {
+			hash: moralisResult[0].path.split("/")[4],
+		};
+	}
 }
